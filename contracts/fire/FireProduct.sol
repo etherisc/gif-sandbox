5e4731766b05a62d8c516a7f49108b7f02a462d7
Christoph Mussenbrock <christoph@etherisc.com>
2023-03-30 20:07:52 +0000

    fix fireproduct.sol

--- contracts/fire/FireProduct.sol
+++ contracts/fire/FireProduct.sol
@@ -6,13 +6,12 @@ import "@etherisc/gif-interface/contracts/components/Product.sol";
 import "./FireOracle.sol";
 
 contract FireProduct is Product {
-
     // constants
     bytes32 public constant VERSION = "0.0.1";
     bytes32 public constant POLICY_FLOW = "PolicyDefaultFlow";
 
     // leads to %5 premiums on object value
-    uint256 public constant OBJECT_VALUE_DIVISOR = 20; 
+    uint256 public constant OBJECT_VALUE_DIVISOR = 20;
 
     // payout specs
     uint256 public constant PAYOUT_FACTOR_MEDIUM = 5;
@@ -22,17 +21,35 @@ contract FireProduct is Product {
 
     // variables
     // TODO should be framework feature
-    bytes32 [] private _applications; // useful for debugging, might need to get rid of this
+    bytes32[] private _applications; // useful for debugging, might need to get rid of this
     uint256 public _oracleId;
 
     mapping(string => bool) public activePolicy;
 
     // events
-    event LogFirePolicyCreated(bytes32 processId, address policyHolder, uint256 sumInsured, string objectName);
+    event LogFirePolicyCreated(
+        bytes32 processId,
+        address policyHolder,
+        uint256 sumInsured,
+        string objectName
+    );
     event LogFirePolicyExpired(string objectName, bytes32 processId);
-    event LogFireOracleCallbackReceived(uint256 requestId, bytes32 processId, bytes fireCategory);
-    event LogFireClaimConfirmed(bytes32 processId, uint256 claimId, uint256 payoutAmount);
-    event LogFirePayoutExecuted(bytes32 processId, uint256 claimId, uint256 payoutId, uint256 payoutAmount);
+    event LogFireOracleCallbackReceived(
+        uint256 requestId,
+        bytes32 processId,
+        bytes fireCategory
+    );
+    event LogFireClaimConfirmed(
+        bytes32 processId,
+        uint256 claimId,
+        uint256 payoutAmount
+    );
+    event LogFirePayoutExecuted(
+        bytes32 processId,
+        uint256 claimId,
+        uint256 payoutId,
+        uint256 payoutAmount
+    );
 
     constructor(
         bytes32 productName,
@@ -40,45 +57,44 @@ contract FireProduct is Product {
         uint256 oracleId,
         uint256 riskpoolId,
         address registry
-    )
-        Product(productName, token, POLICY_FLOW, riskpoolId, registry)
-    {
+    ) Product(productName, token, POLICY_FLOW, riskpoolId, registry) {
         _oracleId = oracleId;
     }
 
-    function applications() external view returns(uint256 numberOfApplications) {
+    function applications()
+        external
+        view
+        returns (uint256 numberOfApplications)
+    {
         return _applications.length;
     }
 
-    function getApplicationId(uint256 idx) external view returns(bytes32 processId) {
-        require(idx < _applications.length, "ERROR:FI-001:APPLICATION_INDEX_TOO_LARGE");
+    function getApplicationId(
+        uint256 idx
+    ) external view returns (bytes32 processId) {
+        require(
+            idx < _applications.length,
+            "ERROR:FI-001:APPLICATION_INDEX_TOO_LARGE"
+        );
         return _applications[idx];
     }
 
-    function decodeApplicationParameterFromData(bytes memory data) 
-        public
-        pure
-        returns(string memory objectName)
-    {
+    function decodeApplicationParameterFromData(
+        bytes memory data
+    ) public pure returns (string memory objectName) {
         return abi.decode(data, (string));
     }
 
-
-    function encodeApplicationParametersToData(string memory objectName)
-        public
-        pure
-        returns(bytes memory data)
-    {
+    function encodeApplicationParametersToData(
+        string memory objectName
+    ) public pure returns (bytes memory data) {
         return abi.encode(objectName);
     }
 
     function applyForPolicy(
         string memory objectName,
         uint256 objectValue
-    )
-        external 
-        returns (bytes32 processId, uint256 requestId) 
-    {
+    ) external returns (bytes32 processId, uint256 requestId) {
         // Validate input parameters
         require(objectValue > 0, "ERROR:FI-010:OBJECT_VALUE_ZERO");
         require(!activePolicy[objectName], "ERROR:FI-011:ACTIVE_POLICY_EXISTS");
@@ -88,17 +104,20 @@ contract FireProduct is Product {
         uint256 premiumAmount = calculatePremium(objectValue);
         uint256 sumInsuredAmount = objectValue;
         bytes memory metaData = "";
-        bytes memory applicationData = encodeApplicationParametersToData(objectName);
-        
+        bytes memory applicationData = encodeApplicationParametersToData(
+            objectName
+        );
+
         processId = _newApplication(
-            policyHolder, 
-            premiumAmount, 
-            sumInsuredAmount, 
-            metaData, 
-            applicationData);
+            policyHolder,
+            premiumAmount,
+            sumInsuredAmount,
+            metaData,
+            applicationData
+        );
 
         _underwrite(processId);
-        
+
         // Update activ state for object
         activePolicy[objectName] = true;
         _applications.push(processId);
@@ -111,21 +130,26 @@ contract FireProduct is Product {
             _oracleId
         );
 
-        emit LogFirePolicyCreated(processId, policyHolder, sumInsuredAmount, objectName);
+        emit LogFirePolicyCreated(
+            processId,
+            policyHolder,
+            sumInsuredAmount,
+            objectName
+        );
     }
 
-
-    function calculatePremium(uint256 objectValue) public pure returns(uint256 premiumAmount) {
+    function calculatePremium(
+        uint256 objectValue
+    ) public pure returns (uint256 premiumAmount) {
         return objectValue / OBJECT_VALUE_DIVISOR;
     }
 
-    function expirePolicy(bytes32 processId)
-        external
-        onlyOwner
-    {
-        // Get policy data 
+    function expirePolicy(bytes32 processId) external onlyOwner {
+        // Get policy data
         IPolicy.Application memory application = _getApplication(processId);
-        (string memory objectName) = decodeApplicationParameterFromData(application.data);
+        string memory objectName = decodeApplicationParameterFromData(
+            application.data
+        );
 
         // Validate input parameter
         require(activePolicy[objectName], "ERROR:FI-005:EXPIRED_POLICY");
@@ -136,44 +160,52 @@ contract FireProduct is Product {
         emit LogFirePolicyExpired(objectName, processId);
     }
 
-    function oracleCallback(uint256 requestId, bytes32 policyId, bytes calldata response)
-        external
-        onlyOracle
-    {
+    function oracleCallback(
+        uint256 requestId,
+        bytes32 policyId,
+        bytes calldata response
+    ) external onlyOracle {
         emit LogFireOracleCallbackReceived(requestId, policyId, response);
 
         // Get policy data for oracle response
-        bytes memory applicationData = _getApplication(policyId).data;
-        (
-            address payable policyHolder, 
-            string memory objectName, 
-            uint256 premium
-        ) = abi.decode(applicationData, (address, string, uint256));
+        /*
+struct Application {
+        ApplicationState state;
+        uint256 premiumAmount;
+        uint256 sumInsuredAmount;
+        bytes data; 
+        uint256 createdAt;
+        uint256 updatedAt;
+    }
+*/
+
+        IPolicy.Application memory applicationData = _getApplication(policyId);
+        uint256 premium = applicationData.premiumAmount;
+        string memory objectName = decodeApplicationParameterFromData(
+            applicationData.data
+        );
+        address payable policyHolder = payable(_getMetadata(policyId).owner);
 
         // Validate input parameter
         require(activePolicy[objectName], "ERROR:FI-006:EXPIRED_POLICY");
 
         // Get oracle response data
-        (bytes1 fireCategory) = abi.decode(response, (bytes1));
+        bytes1 fireCategory = abi.decode(response, (bytes1));
 
-        // Claim handling based on reponse to greeting provided by oracle 
+        // Claim handling based on reponse to greeting provided by oracle
         _handleClaim(policyId, policyHolder, premium, fireCategory);
     }
 
-
-    function getOracleId() external view returns(uint256 oracleId) {
+    function getOracleId() external view returns (uint256 oracleId) {
         return _oracleId;
     }
 
-
     function _handleClaim(
-        bytes32 policyId, 
-        address payable policyHolder, 
-        uint256 premium, 
+        bytes32 policyId,
+        address payable policyHolder,
+        uint256 premium,
         bytes1 fireCategory
-    ) 
-        internal 
-    {
+    ) internal {
         uint256 payoutAmount = _calculatePayoutAmount(premium, fireCategory);
 
         // no claims handling for payouts == 0
@@ -186,18 +218,22 @@ contract FireProduct is Product {
             uint256 payoutId = _newPayout(policyId, claimId, payoutAmount, "");
             _processPayout(policyId, payoutId);
 
-            emit LogFirePayoutExecuted(policyId, claimId, payoutId, payoutAmount);
+            emit LogFirePayoutExecuted(
+                policyId,
+                claimId,
+                payoutId,
+                payoutAmount
+            );
         }
     }
 
-    function _calculatePayoutAmount(uint256 premium, bytes1 fireCategory) 
-        internal 
-        pure 
-        returns(uint256 payoutAmount) 
-    {
-        if (fireCategory == 'M') {
+    function _calculatePayoutAmount(
+        uint256 premium,
+        bytes1 fireCategory
+    ) internal pure returns (uint256 payoutAmount) {
+        if (fireCategory == "M") {
             payoutAmount = PAYOUT_FACTOR_MEDIUM * premium;
-        } else if (fireCategory == 'L') { 
+        } else if (fireCategory == "L") {
             payoutAmount = PAYOUT_FACTOR_LARGE * premium;
         } else {
             // small fires considered below deductible, no payout
